d39fe5229ac755c9d5a874c388d214d484599c78
adem951 <100589825+adem951@users.noreply.github.com>
2023-12-04 10:22:34 -0500

    Update data.js

--- data/data.js
+++ data/data.js
@@ -3,7 +3,7 @@ const packages = [
         package: "Audi RS6",
         usage: "the core language tools and it's available by default",
         link: "https://fr.wikipedia.org/wiki/Audi_RS6",
-        note: "<em>For further reference and documentation, you can see  <a href='https://fr.wikipedia.org/wiki/Liste_des_voitures_de_s%C3%A9rie_les_plus_rapides_au_monde' target='_blank'>here</a>, more detailed information about car models and their features.</em>",
+        description: "<em>For further reference and documentation, you can see  <a href='https://fr.wikipedia.org/wiki/Liste_des_voitures_de_s%C3%A9rie_les_plus_rapides_au_monde' target='_blank'>here</a>, more detailed information about car models and their features.</em>",
         image: "images/AUDI-RS6.1.jpg",
         imagelink: "https://fr.wikipedia.org/wiki/Audi_RS6"
     },
@@ -11,7 +11,7 @@ const packages = [
         package: "Porsche 911",
         usage: "classes and interface relating to fonts",
         link: "https://fr.wikipedia.org/wiki/Porsche_911",
-        note: "<em>For further reference and documentation, you can see  <a href='https://fr.wikipedia.org/wiki/Liste_des_voitures_de_s%C3%A9rie_les_plus_rapides_au_monde' target='_blank'>here</a>, more detailed information about car models and their features.</em>",
+        description: "<em>For further reference and documentation, you can see  <a href='https://fr.wikipedia.org/wiki/Liste_des_voitures_de_s%C3%A9rie_les_plus_rapides_au_monde' target='_blank'>here</a>, more detailed information about car models and their features.</em>",
         image: "images/porsche_911.jpg",
         imagelink: "https://fr.wikipedia.org/wiki/Porsche_911"
     },
@@ -27,7 +27,7 @@ const packages = [
         package: "2023 Mercedes-Benz SL-Class",
         usage: "classes that are fundamental to the design of the Java programming language",
         link: "https://en.wikipedia.org/wiki/Mercedes-Benz_SL-Class",
-        note: "<em>For further reference and documentation, you can see  <a href='https://fr.wikipedia.org/wiki/Liste_des_voitures_de_s%C3%A9rie_les_plus_rapides_au_monde' target='_blank'>here</a>, more detailed information about car models and their features.</em>",
+        description: "<em>For further reference and documentation, you can see  <a href='https://fr.wikipedia.org/wiki/Liste_des_voitures_de_s%C3%A9rie_les_plus_rapides_au_monde' target='_blank'>here</a>, more detailed information about car models and their features.</em>",
         image: "images/2023-mercedes-benz.jpg",
         imagelink: "https://en.wikipedia.org/wiki/Mercedes-Benz_SL-Class"
     },
@@ -35,7 +35,7 @@ const packages = [
         package: "2024 Porsche Boxster",
         usage: "contains classes and interfaces that support a generic API for random number generation",
         link: "https://cars.usnews.com/cars-trucks/porsche/boxster/photos-exterior",
-        note: "<em>For further reference and documentation, you can see  <a href='https://fr.wikipedia.org/wiki/Liste_des_voitures_de_s%C3%A9rie_les_plus_rapides_au_monde' target='_blank'>here</a>, more detailed information about car models and their features.</em>",
+        description: "<em>For further reference and documentation, you can see  <a href='https://fr.wikipedia.org/wiki/Liste_des_voitures_de_s%C3%A9rie_les_plus_rapides_au_monde' target='_blank'>here</a>, more detailed information about car models and their features.</em>",
         image: "images/718_cayman.jpg",
         imagelink: "https://cars.usnews.com/cars-trucks/porsche/boxster/photos-exterior"
     },
